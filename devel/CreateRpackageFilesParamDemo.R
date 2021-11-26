cc7a5764f74d668a44c4a9061f104507121a88bc
fercol <colchero@imada.sdu.dk>
2021-11-26 16:19:26 +0100

    Script to create the R package paramDemo

--- devel/CreateRpackageFilesParamDemo.R
+++ devel/CreateRpackageFilesParamDemo.R
@@ -8,13 +8,15 @@ mainDir <- "~/FERNANDO/PROJECTS/4.PACKAGES/"
 pkgName <- "paramDemo" 
 
 # Code name:
-codeFile <- "paramDemo"
+scriptFile <- "paramDemo"
 
 # Create .Rd (help) files (requires a code file):
-CreateRdFiles(pkgName = pkgName, mainDir = mainDir, codeFile = codeFile,
+CreateRdFiles(pkgName = pkgName, mainDir = mainDir, scriptFile = scriptFile,
               authorNames = "Fernando Colchero", 
               authorEmails = "colchero@imada.sdu.dk")
 
 # Create namespace:
-CreateNamespace(pkgName = pkgName, mainDir = mainDir, codeFile = codeFile)
+CreateNamespace(pkgName = pkgName, mainDir = mainDir, scriptFile = scriptFile)
 
+# Create description file:
+CreatePkgDescrip(pkgName = pkgName, mainDir = mainDir, title = "Parametric and non-parametric demographic functions and applications.", version = "1.0.0", authors = "Fernando Colchero", maintainer = "Fernando Colchero <colchero@imada.sdu.dk>")
